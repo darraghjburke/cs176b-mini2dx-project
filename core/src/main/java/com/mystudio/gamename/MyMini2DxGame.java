0db88580b7ced4f85b98986bc563e51693d5d94e
DARRAGH BURKE <darragh@ucsb.edu>
2020-01-17 15:20:12 -0800

    add up and down keys

--- core/src/main/java/com/mystudio/gamename/MyMini2DxGame.java
+++ core/src/main/java/com/mystudio/gamename/MyMini2DxGame.java
@@ -2,6 +2,7 @@ package com.mystudio.gamename;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
@@ -29,6 +30,12 @@ public class MyMini2DxGame extends BasicGame {
       if (Gdx.input.isKeyPressed(Keys.DPAD_LEFT)) {
         x -= delta * 300;
       }
+      if (Gdx.input.isKeyPressed(Keys.DPAD_UP)) {
+        y -= delta * 300;
+      }
+      if (Gdx.input.isKeyPressed(Keys.DPAD_DOWN)) {
+        y += delta * 300;
+      }
     }
     
     @Override
@@ -39,7 +46,8 @@ public class MyMini2DxGame extends BasicGame {
     @Override
     public void render(Graphics g) {
         //g.drawTexture(texture, 0f, 0f);
-        //g.setColor();
+        g.setColor(Color.WHITE);
+        g.setLineHeight(1);
         g.drawRect(x, y, 128, 128);
     }
 }
